9f30f5e20c2348a7a8548cf8aca4fb90cf22c911
niwolos <hansguempert@gmx.de>
2021-07-02 17:51:35 +0200

    Implemented Part 2 of Pokefight (FrontEnd)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import PokemonDetails from "./PokemonDetails";
+import axios from "axios";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState();
   useEffect(() => {
-    fetch("/api/pokemon/12")
+    /*
+    fetch("http://localhost:3001/pokemon")
       .then((res) => {
         res.json();
-        console.log(res);
+        console.log({ data: res.data});
+        setPokemonData(res.body);
       })
-      .then((data) => console.log(data));
+      */
+    axios
+      .get("http://localhost:3001/pokemon")
+      .then((res) => {
+        console.log({ pokemon_data: res.data });
+        setPokemonData(res.data);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
   }, []);
 
+  if (pokemonData) {
+    return (
+      <>
+        <Route exact path="/">
+          {pokemonData.map((onePokemon) => {
+            const pokemonLink = `/pokemon/${onePokemon.id}`;
+            return (
+              <p>
+                <a href={pokemonLink}>{onePokemon.name.english}</a>
+              </p>
+            );
+          })}
+          ;
+        </Route>
+        <Route exact path="/pokemon/:id">
+            <PokemonDetails pokemonList={pokemonData}></PokemonDetails>
+          </Route>
+        <Route path="/pokemon/:id/:info">
+            <PokemonDetails pokemonList={pokemonData}></PokemonDetails>
+          </Route>
+      </>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>404. You don't have pokemon data available yet.</p>
+        </header>
+      </div>
+    </>
   );
 }
 
---
+++ src/PokemonDetails.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function PokemonDetails({ pokemonList }) {
+  const { id } = useParams();
+  const { info } = useParams();
+  const [pokemonById, setPokemonById] = useState();
+
+  useEffect(() => {
+    if (!info) {
+      axios
+        .get(`http://localhost:3001/pokemon/${id}`)
+        .then((res) => {
+          console.log({ pokemon_data: res.data });
+          setPokemonById(res.data);
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
+    } else {
+      axios
+        .get(`http://localhost:3001/pokemon/${id}/${info}`)
+        .then((res) => {
+          console.log({ pokemon_data: res.data });
+          setPokemonById(res.data);
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
+    }
+  }, []);
+
+  /**
+  if (pokemonList) {
+    const selectedPokemonById = pokemonList.find((pokemon) => {
+      return pokemon.id == id;
+    });
+    */
+
+  if (pokemonById) {
+    if (info) {
+        let infoElement = <p>Invalid Parameter. Please use 'name', 'type' or 'base' in the URL! e.g.: ".../pokemon/id/type/"</p>;
+        switch (info.toLowerCase()) {
+            case "name":
+              infoElement = <p>Name: {pokemonById.name.english}</p>;
+              break;
+            case "type":
+                infoElement = <p>Name: {JSON.stringify(pokemonById.type)}</p>;
+              break;
+            case "base":
+                infoElement = <p>Name: {JSON.stringify(pokemonById.base)}</p>;
+              break;
+            default:
+              break;
+        }
+    
+        return (
+            <>
+              <a href="/">List of Pokemons</a>
+              {infoElement}
+            </>
+          );
+    } else {
+      return (
+        <>
+          <a href="/">List of Pokemons</a>
+          <p>Name: {pokemonById.name.english}</p>
+          <p>Name: {JSON.stringify(pokemonById.type)}</p>
+          <p>Name: {JSON.stringify(pokemonById.base)}</p>
+        </>
+      );
+    }
+  }
+
+  return (
+    <>
+      <>404. No pokemon detailed data available for this id.</>
+    </>
+  );
+}
