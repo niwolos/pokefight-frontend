750e68c24c2ad61401383578844ead4e5e41b138
niwolos <hansguempert@gmx.de>
2021-07-12 12:03:06 +0200

    Adjusted some gameplay view layout & added few states

--- src/PokemonGame.js
+++ src/PokemonGame.js
@@ -2,61 +2,105 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./PokemonGame.css";
 
-class PokemonGame extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {  }
-    }
-    render() { 
-      return ( 
-        <>
-          <div className="totalContainer">
-            <div className="firstColumn">
-                <button className="saveButton">Save Match</button><br/>
-                <label className="winnerLabel">Winner</label>
-                <div className="leftFirstSubContainer">
-                    <div className="leftSecondSubContainer">
-                        <textarea className="idArea"></textarea>
-                        <textarea></textarea>
-                    </div>
-                    <div className="leftThirdSubContainer">
-                        <textarea clasName="resultsOne"></textarea>
-                        <textarea clasName="resultsOne"></textarea>
-                        <textarea clasName="resultsOne"></textarea>
-                        <textarea></textarea>
-                        <img src="https://www.pngkit.com/png/detail/10-103745_report-abuse-cutest-pokemon-picture-ever.png" width="200px"></img>
-                    </div>
-                </div>
+function PokemonGame() {
+  const [pokemonAIdentity, setPokemonAIdentity] = useState({ id: 0 });
+  const [pokemonBIdentity, setPokemonBIdentity] = useState({ id: 1 });
+  const [pokemonAStats, setPokemonAStats] = useState();
+  const [pokemonBStats, setPokemonBStats] = useState();
+  const [currentTurn, setCurrentTurn] = useState("0");
+  const [gameActive, setGameActive] = useState("false");
+  
+
+  const updatePokemonAIdentity = (e) => {
+    console.log(
+      `Changing PokemonA's Identity param: ${e.target.name} value to: ${e.target.value}`
+    );
+    setPokemonAIdentity({
+      ...pokemonAIdentity,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const updatePokemonBIdentity = (e) => {
+    console.log(
+      `Changing PokemonB's Identity param: ${e.target.name} value to: ${e.target.value}`
+    );
+    setPokemonBIdentity({
+      ...pokemonBIdentity,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <div className="totalContainer">
+        <div className="firstColumn">
+          <button className="saveButton">Save Match</button>
+          <br />
+          <label className="winnerLabel">Winner</label>
+          <div className="leftFirstSubContainer">
+            <div className="leftSecondSubContainer">
+              <label text="id" />
+              <textarea className="id1" placeholder="id.."></textarea>
+              <label text="name" />
+              <textarea className="name1" placeholder="name.."></textarea>
             </div>
-            <div className="secondColumn">
-               <h1>PokéFight?</h1>
-               <div>
-                   <button>Next turn</button>
-                   <label>Turn</label>
-                   <button>x</button>
-               </div>
-            </div>
-            <div className="thirdColumn">
-            <button className="randomButton">Randomize</button><br/>
-                <label className="winnerLabel">Winner</label>
-                <div className="leftFirstSubContainer">
-                    <div className="leftSecondSubContainer">
-                        <textarea className="idArea"></textarea>
-                        <textarea></textarea>
-                    </div>
-                    <div className="leftThirdSubContainer">
-                        <textarea clasName="resultsOne"></textarea>
-                        <textarea clasName="resultsOne"></textarea>
-                        <textarea clasName="resultsOne"></textarea>
-                        <textarea></textarea>
-                        <img src="https://www.pngkit.com/png/detail/10-103745_report-abuse-cutest-pokemon-picture-ever.png" width="200px"></img>
-                    </div>
-                </div>
+            <div className="leftThirdSubContainer">
+              <label text="HP" />
+              <textarea clasName="hp_1"></textarea>
+              <label text="ATK" />
+              <textarea clasName="atk_1"></textarea>
+              <label text="DEF" />
+              <textarea clasName="def_1"></textarea>
+              <label text="SPD" />
+              <textarea clasName="speed_1"></textarea>
+              <img
+                src="https://www.pngkit.com/png/detail/10-103745_report-abuse-cutest-pokemon-picture-ever.png"
+                width="200px"
+                alt="Pokemon img not found"
+              ></img>
             </div>
           </div>
-        </>
-       );
-    }
-  }
-   
-  export default PokemonGame;
+        </div>
+        <div className="secondColumn">
+          <h1>PokéFight?</h1>
+          <div>
+            <button>Next turn</button>
+            <label>Turn</label>
+            <button>x</button>
+          </div>
+        </div>
+        <div className="thirdColumn">
+          <button className="randomButton">Randomize</button>
+          <br />
+          <label className="winnerLabel">Winner</label>
+          <div className="rightFirstSubContainer">
+            <div className="rightSecondSubContainer">
+              <label text="id" />
+              <textarea className="id2" placeholder="id.."></textarea>
+              <label text="name" />
+              <textarea className="name2" placeholder="name.."></textarea>
+            </div>
+            <div className="rightThirdSubContainer">
+              <label text="HP" />
+              <textarea clasName="hp_2"></textarea>
+              <label text="ATK" />
+              <textarea clasName="atk_2"></textarea>
+              <label text="DEF" />
+              <textarea clasName="def_2"></textarea>
+              <label text="SPD" />
+              <textarea clasName="speed_2"></textarea>
+              <img
+                src="https://www.pngkit.com/png/detail/10-103745_report-abuse-cutest-pokemon-picture-ever.png"
+                width="200px"
+                alt="Pokemon img not found"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PokemonGame;
